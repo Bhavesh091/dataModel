3bae9c37a910ce13a828e0132f8cf8cd6e7fba6d
Bhavesh Odedara <bhaveshodedra091@gmail.com>
2018-10-12 14:41:02 +0530

    Update ModelDataset.swift

--- ModelDataset.swift
+++ ModelDataset.swift
@@ -10,13 +10,13 @@ import Foundation
 // MARK: - Model Dataset Methods
 //-------------------------------------------------------------
 
-func genericModelDataset<T: Codable>(dataOfModel: Data, model: T.Type) -> (response:T?, error: error?) {
+func genericModelDataset<T: Codable>(dataOfModel: Data, model: T.Type) -> T? {
     
     do {
         let modelData = try JSONDecoder().decode(T.self, from: dataOfModel)
-        return (modelData,nil)
+        return modelData
     }
     catch {
-        return (nil,error)
+        return nil
     }
 }
